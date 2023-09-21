6ef41bdf2b9f7bcac679223746719bfc21f560e4
Nastenko Peter <source-2306@mail.ru>
2023-09-21 16:53:01 +0300

    Сохранение все редмишки в один файл

--- start.py
+++ start.py
@@ -3,6 +3,7 @@ import os
 from dict_path_name import d
 
 path = 'd:\\ROBO_ONLINE\\ALL_FILE'
+d = {}
 
 
 def write_text(new_file: str, text: str):
@@ -12,23 +13,26 @@ def write_text(new_file: str, text: str):
     :param text: текст который нужно записать
     """
     with open(new_file, 'a') as n_f:
-        n_f.write('\n')
         n_f.write(text)
+        # n_f.write('\n')
         n_f.close()
 
 
-def read_readme(path_name_readme: str):
+def read_readme(path_name_readme: str, name_readme: str):
     """
-    Чтение README.txt
+    Чтение README.txt перекодировка в utf-8
     :param path_name_readme: имя файла для чтения Readme
-    :return list: Возвращает прочитанный файл
+    :return list: Возвращает текст файла
     """
-    with open(path_name_readme, 'r', encoding="utf-8") as file:
-        try:
-            return file.read()
-        except UnicodeDecodeError:
-            with open(path_name_readme, 'r') as file:
+    if name_readme.lower() == 'readme' or name_readme.lower() == 'readme.txt':
+        with open(path_name_readme, 'r', encoding="utf-8") as file:
+            try:
                 return file.read()
+            except UnicodeDecodeError:
+                with open(path_name_readme, 'r') as file:
+                    return file.read()
+    else:
+        return name_readme
 
 
 def list_name_build(path):
@@ -61,7 +65,7 @@ def dict_write_path_name(path):
             if 'readme' in content:
                 content.remove('readme')
                 # content.insert(0, 'readme') # вставка вначало
-                content.append('readme.txt')
+                content.append('readme')
             d[path] = content
     try:
         os.remove('dict_path_name.py')
@@ -70,4 +74,37 @@ def dict_write_path_name(path):
         write_text(f"dict_path_name.py", f'd = {d}')
 
 
-dict_write_path_name(path)
+def save_all_readme(name_file_out: str, dict_file: dict):
+    """
+    Запись собранyой readme с
+    :param name_file_out: Имя файла
+    """
+    sorted_dict_file = dict(sorted(dict_file.items(), reverse=True))
+    try:
+        os.remove(name_file_out)
+    except FileNotFoundError:
+        pass
+    date_path_one = ''
+    for keys, values_all in sorted_dict_file.items():  # цикл по ключам
+        date_path_two = keys[24:32]
+        path_keys = keys
+        if date_path_one != date_path_two:
+            write_text(name_file_out, f'----------------{date_path_two}-----------------')
+            date_path_one = date_path_two
+        for values_one in values_all:  # цикл по значениям
+            if values_one != 'readme' and values_one != 'readme.txt':
+                write_text(name_file_out, '\n')
+                path_keys = f'{keys}\{values_one}'
+                write_text(name_file_out, f'ДОБАВЛЕН ФАЙЛ:{values_one} ')
+            else:
+                write_text(name_file_out, '\n')
+                path_keys = f'{keys}\{values_one}'
+                write_text(name_file_out, read_readme(path_keys, values_one))
+        write_text(name_file_out, '\n')
+        # write_text(name_file_out, '\n')
+    dict_write_path_name(path)
+
+
+if __name__ == "__main__":
+    # dict_write_path_name(path)
+    save_all_readme('2.txt', d)
